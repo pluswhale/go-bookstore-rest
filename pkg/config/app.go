00a364ca2837fc18eeec210d73f49ac1f5587ae3
Egor Dultsev <listenmy@yandex.ru>
2022-06-04 14:17:20 +0300

    first commit

---
+++ pkg/config/app.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	dsn := "host=localhost user=postgres password=123 dbname=bookstore port=5432 sslmode=disable TimeZone=Europe/Moscow"
+	d, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
